e333efaf493142230d2ceaaa505c6302707a75ad
Facultate <badeanarcis@gmail.com>
2022-04-17 17:13:07 +0300

    Update

---
+++ CLOUD/CLOUD/Migrations/20220417141215_profesie.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CLOUD.Migrations
+{
+    public partial class profesie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Profestie",
+                table: "Pacienti",
+                newName: "Profesie");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Profesie",
+                table: "Pacienti",
+                newName: "Profestie");
+        }
+    }
+}
